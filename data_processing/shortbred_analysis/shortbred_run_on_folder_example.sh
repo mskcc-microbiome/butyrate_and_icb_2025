222082ed3d0e720a9ff41b93eac0ff02b284c26c
baichom1 <baichom1@mskcc.org>
2025-03-17 17:09:03 -0400

    fixing the folder submission script

--- data_processing/shortbred_analysis/shortbred_run_on_folder_example.sh
+++ data_processing/shortbred_analysis/shortbred_run_on_folder_example.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
-FOLDER=
-THREADS=10
+FOLDER="<replace me>"
+OUTPUT_DIR="<replace me>"
+PARAM_SOURCE="<replace me>"/butyrate_and_icb_2025/data_processing/shortbred_analysis/config_params.sh
+THREADS=="<replace me (maybe 10?)>"
 
-while read line; do 
+if [ ! -d $OUTPUT_DIR ]; then mkdir $OUTPUT_DIR; fi
+if [ ! -d "$OUTPUT_DIR/logs" ]; then mkdir $OUTPUT_DIR/logs; fi
+if [ ! -d "$OUTPUT_DIR/jobs" ]; then mkdir $OUTPUT_DIR/jobs; fi
 
-    echo "Submitting pipeline job for ${sample_name}"
-    cp shortbred_template.sh ${JOB_DIR}shortbred_${sample_name}.sh
-
-    sed -i -r "s|^(input=).*|\1${sample_name}|" ${JOB_DIR}shortbred_${sample_name}.sh
-    sed -i -r "s|^(output=).*|\1${values[1]}|" ${JOB_DIR}shortbred_${sample_name}.sh
-    sed -i -r "s|^(tmp_dir=).*|\1${values[2]}|" ${JOB_DIR}shortbred_${sample_name}.sh
-    sed -i -r "s|^(threads=).*|\1${THREADS}|" ${JOB_DIR}shortbred_${sample_name}.sh
-    sed -i -r "s|^(param_source=).*|\1${values[3]}|" ${JOB_DIR}shortbred_${sample_name}.sh
-
-    sed -i -r "s|^(#BSUB -o .*).*|\1${output_log}|" ${JOB_DIR}shortbred_${sample_name}.sh
-    sed -i -r "s|^(#BSUB -eo ).*|\1${error_log}|" ${JOB_DIR}shortbred_${sample_name}.sh
-    sed -i -r "s|^(#BSUB -J ).*|\1${jobname}|" ${JOB_DIR}shortbred_${sample_name}.sh
+for input in $FOLDER/*fastq.gz; do
+    filebase=$(basename $input)
+    sample="${filebase%%.fastq.gz}"
+    output=$OUTPUT_DIR/${sample}_butyrate.txt
+    tmp_dir=$OUTPUT_DIR/${sample}_tmp/
+    JOB=$OUTPUT_DIR/jobs/${sample}_shortbred.sh
+    output_log=$OUTPUT_DIR/logs/${sample}.out
+    error_log=$OUTPUT_DIR/logs/${sample}.err
+    job_name=shortbred_${sample}
     
-    sed -i -r "s|^(#BSUB -n ).*|\1${THREADS}|" ${JOB_DIR}shortbred_${sample_name}.sh
-    bsub < ${JOB_DIR}shortbred_${sample_name}.sh
+    cp shortbred_template.sh $JOB
+    sed -i -r "s|^(input=).*|\1${input}|" $JOB
+    sed -i -r "s|^(output=).*|\1${output}|" $JOB
+    sed -i -r "s|^(tmp_dir=).*|\1${tmp_dir}|" $JOB
+    sed -i -r "s|^(param_source=).*|\1${PARAM_SOURCE}|" $JOB
+    sed -i -r "s|^(threads=).*|\1${THREADS}|" $JOB
 
-done <  ${sample_location_file}
+    
+    sed -i -r "s|^(#BSUB -o .*).*|\1${output_log}|" $JOB
+    sed -i -r "s|^(#BSUB -eo ).*|\1${error_log}|" $JOB
+    sed -i -r "s|^(#BSUB -J ).*|\1${job_name}|" $JOB
+    sed -i -r "s|^(#BSUB -n ).*|\1${THREADS}|" $JOB
+
+    bsub < $JOB
+done
