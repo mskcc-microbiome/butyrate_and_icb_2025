afa3773e313ddcd0edba78979cabf6dd75198f34
baichom1 <baichom1@mskcc.org>
2025-03-17 13:00:12 -0400

    fixing template

--- data_processing/shortbred_analysis/shortbred_run_on_folder_example.sh
+++ data_processing/shortbred_analysis/shortbred_run_on_folder_example.sh
@@ -1,38 +1,24 @@
 #!/bin/bash
 
 FOLDER=
+THREADS=10
 
 while read line; do 
 
-    IFS=",";values=($line);IFS='***';
+    echo "Submitting pipeline job for ${sample_name}"
+    cp shortbred_template.sh ${JOB_DIR}shortbred_${sample_name}.sh
 
-    sample_name="${values[0]}"
-    output_log="${LOG_DIR}caz_pipeline_${sample_name}.stdout"
-    error_log="${LOG_DIR}caz_pipeline_${sample_name}.stderr"
-    jobname=pipeline_${sample_name}
+    sed -i -r "s|^(input=).*|\1${sample_name}|" ${JOB_DIR}shortbred_${sample_name}.sh
+    sed -i -r "s|^(output=).*|\1${values[1]}|" ${JOB_DIR}shortbred_${sample_name}.sh
+    sed -i -r "s|^(tmp_dir=).*|\1${values[2]}|" ${JOB_DIR}shortbred_${sample_name}.sh
+    sed -i -r "s|^(threads=).*|\1${THREADS}|" ${JOB_DIR}shortbred_${sample_name}.sh
+    sed -i -r "s|^(param_source=).*|\1${values[3]}|" ${JOB_DIR}shortbred_${sample_name}.sh
 
-    prokka_sample_dir=${PROKKA_DIR}${sample_name}/
-    dbcan_sample_dir=${DBCAN_DIR}${sample_name}/
-    if [ ! -d $dbcan_sample_dir ]; then
+    sed -i -r "s|^(#BSUB -o .*).*|\1${output_log}|" ${JOB_DIR}shortbred_${sample_name}.sh
+    sed -i -r "s|^(#BSUB -eo ).*|\1${error_log}|" ${JOB_DIR}shortbred_${sample_name}.sh
+    sed -i -r "s|^(#BSUB -J ).*|\1${jobname}|" ${JOB_DIR}shortbred_${sample_name}.sh
+    
+    sed -i -r "s|^(#BSUB -n ).*|\1${THREADS}|" ${JOB_DIR}shortbred_${sample_name}.sh
+    bsub < ${JOB_DIR}shortbred_${sample_name}.sh
 
-        echo "Submitting pipeline job for ${sample_name}"
-        cp run_pipeline.sh ${JOB_DIR}caz_pipeline_${sample_name}.sh
-
-        sed -i -r "s|^(sample_name=).*|\1${sample_name}|" ${JOB_DIR}caz_pipeline_${sample_name}.sh
-        sed -i -r "s|^(r1=).*|\1${values[1]}|" ${JOB_DIR}caz_pipeline_${sample_name}.sh
-        sed -i -r "s|^(r2=).*|\1${values[2]}|" ${JOB_DIR}caz_pipeline_${sample_name}.sh
-        sed -i -r "s|^(assembly=).*|\1${values[3]}|" ${JOB_DIR}caz_pipeline_${sample_name}.sh
-
-        sed -i -r "s|^(prokka_dir=).*|\1${prokka_sample_dir}|" ${JOB_DIR}caz_pipeline_${sample_name}.sh
-        sed -i -r "s|^(dbcan_dir=).*|\1${dbcan_sample_dir}|" ${JOB_DIR}caz_pipeline_${sample_name}.sh
-
-        sed -i -r "s|^(#BSUB -o .*).*|\1${output_log}|" ${JOB_DIR}caz_pipeline_${sample_name}.sh
-        sed -i -r "s|^(#BSUB -eo ).*|\1${error_log}|" ${JOB_DIR}caz_pipeline_${sample_name}.sh
-        sed -i -r "s|^(#BSUB -J ).*|\1${jobname}|" ${JOB_DIR}caz_pipeline_${sample_name}.sh
-        bsub < ${JOB_DIR}caz_pipeline_${sample_name}.sh
-    else
-        echo "dbcan dir for ${sample_name} already exists, skipping."
-    fi
-
-   
 done <  ${sample_location_file}
--- data_processing/shortbred_analysis/shortbred_template.sh
+++ data_processing/shortbred_analysis/shortbred_template.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-#BSUB -J test
-#BSUB -n 10
+#BSUB -J 
+#BSUB -n 
 #BSUB -R rusage[mem=4]
 #BSUB -W 24:00
-#BSUB -o /data/peledj/baichom1/Projects/test_shortbred_analysis/test.o
-#BSUB -eo /data/peledj/baichom1/Projects/test_shortbred_analysis/test.e
+#BSUB -o 
+#BSUB -eo 
 
 input=unknown
 output=unknown
 tmp_dir=unknown
+param_source=unknown
 threads=1
-param_source=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-echo $param_source
-source $param_source/config_params.sh
-echo $QUANTIFY_SCRIPT
-echo "hi"
+source $param_source
 
-#python $QUANTIFY_SCRIPT --markers $MARKER_FASTQ --search_program usearch \
-#            --usearch $USEARCH --threads $threads \
-#            --tmp $tmp_dir --wgs $input --results $output
+
+python $QUANTIFY_SCRIPT --markers $MARKER_FASTQ --search_program usearch \
+            --usearch $USEARCH --threads $threads \
+            --tmp $tmp_dir --wgs $input --results $output
