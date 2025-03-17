41ea2288b10c629e13175f95fb26352479710165
baichom1 <baichom1@mskcc.org>
2025-03-17 06:49:49 -0400

    adding config example

---
+++ data_processing/shortbred_analysis/config_params.sh
@@ -0,0 +1,3 @@
+QUANTIFY_SCRIPT="(the path to the shortBRED quantify_script)"
+MARKER_FASTQ="(path to the marker fastq we provided (or updated marker.fastq if using your own reference))."
+USEARCH="the path to your usearch installation."
